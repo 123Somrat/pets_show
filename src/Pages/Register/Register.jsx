e7d1dfa30abf0b7d96aed0d39f16c4afcc756244
123Somrat <jafaruddin875@gmail.com>
2023-10-07 14:55:21 +0200

    getting user data

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,35 +1,47 @@
 import React from 'react'
 
 export default function Register() {
+
+
+ const handleSubmit = (e)=>{
+    let form = new FormData(e.currentTarget)
+    let  name = form.get("name");
+    let email = form.get("email");
+    let password =  form.get("password");
+    console.log(name,email,password)
+      e.preventDefault()
+ }
+
+
   return (
     <div className="hero min-h-screen bg-base-200">
     <div className="hero-content flex-col lg:flex-row-reverse">
      
       <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 rounded">
-        <form className="card-body">
+        <form className="card-body" onSubmit={handleSubmit}>
         <div className="form-control">
             <label className="label">
               <span className="label-text">Name</span>
             </label>
-            <input type="name" placeholder="name" className="input input-bordered" required />
+            <input type="name" placeholder="name" name="name" className="input input-bordered" required />
           </div>
           <div className="form-control">
             <label className="label">
               <span className="label-text">Email</span>
             </label>
-            <input type="email" placeholder="email" className="input input-bordered" required />
+            <input type="email" placeholder="email" name="email" className="input input-bordered" required />
           </div>
           <div className="form-control">
             <label className="label">
               <span className="label-text">Password</span>
             </label>
-            <input type="password" placeholder="password" className="input input-bordered" required />
+            <input type="password" placeholder="password" name="password" className="input input-bordered" required />
             <label className="label">
               <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
             </label>
           </div>
           <div className="form-control mt-6">
-            <button className="btn btn-primary">Register</button>
+            <button type="submit" className="btn btn-primary">Register</button>
           </div>
         </form>
       </div>
