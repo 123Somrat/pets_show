dd9ebb3c7ab1f52a2eeac6828ad46742b09791a5
123Somrat <jafaruddin875@gmail.com>
2023-10-07 14:19:48 +0200

    implement register and login route

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -3,6 +3,8 @@ import {
   } from "react-router-dom";
 import MainLayOuts from "../MainLayouts.jsx/MainLayOuts";
 import Home from "../Pages/Home/Home";
+import Register from "../Pages/Register/Register";
+import Login from "../Pages/Login/Login";
 
 
 
@@ -16,7 +18,12 @@ const router = createBrowserRouter([
              element:<Home/>
         },
         {
-            
+           path:"/register",
+           element:<Register/>
+        },
+        {
+             path:"/login",
+             element:<Login/>
         }
 
       ]
